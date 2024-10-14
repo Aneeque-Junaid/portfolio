991b7c1c9eb623f30df2153a4ccff9ef4b7ecf50
Aneeque-Junaid <aneequej2002@gmail.com>
2024-10-14 22:46:40 +0500

    Projects Done

--- src/index.js
+++ src/index.js
@@ -13,13 +13,17 @@ import tailwindcss from '../assets/logos/tailwindcss.svg';
 import javascript from '../assets/logos/javascript.svg';
 import react from '../assets/logos/react.svg';
 import redux from '../assets/logos/redux.svg';
+import nextjs from '../assets/logos/nextjs.svg';
 
+import hoobank from "../assets/images/hoobank.png"
+import niketailwind from "../assets/images/niketailwind.png"
 
 export const skills = [
     { name: 'HTML', logo: html },
     { name: 'CSS', logo: css },
     { name: 'JavaScript', logo: javascript }, // Corrected
     { name: 'React', logo: react },           // Corrected
+    { name: 'NextJS', logo: nextjs },
     { name: 'Tailwind CSS', logo: tailwindcss }, // Corrected
     { name: 'Redux', logo: redux },
 ];
@@ -30,21 +34,22 @@ export {Nav, Hero, About, Projects, Skills, Contact, Footer}
 export const projects = [
     {
       id: 1,
-      title: 'Project One',
-      image: 'path/to/project-one-image.jpg', // Replace with your project image path
-      description: 'This is a brief description of Project One.',
+      title: 'Hoobank',
+      link: 'https://hoobank1001.netlify.app/',
+      image: hoobank, 
+      description: 'Hoobank is a modern banking application built with React and Tailwind CSS. It features a sleek user interface that allows users to manage their finances effortlessly, showcasing responsive design and interactive components for an enhanced user experience.',
       techStack: {
-        tech: ["React", "Node.js", "MongoDB", "Express"],
+        tech: ["React", "TailwindCSS"],
       },
     },
     {
       id: 2,
-      title: 'Project Two',
-      image: 'path/to/project-two-image.jpg', // Replace with your project image path
-      description: 'This is a brief description of Project Two.',
+      title: 'Nike Website',
+      link: 'https://shoewebsitenike.netlify.app/',
+      image: niketailwind, 
+      description: "The Nike Website is a visually appealing landing page developed with React and Tailwind CSS. It highlights Nike's latest products with a clean, responsive layout, creating an engaging experience for users.",
       techStack: {
-        tech: ["React", "Node.js", "MongoDB", "Express"],
+        tech: ["React", "TailwindCSS"],
       },
     },
-    // Add more projects as needed
   ];
